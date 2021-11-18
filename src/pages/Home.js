664e3f7c21ac46ccb6823e0a52c45dc739e23103
DaanKorver <daank2000@hotmail.nl>
2021-11-18 14:11:09 +0100

    WIP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
 import {NavLink as Link} from 'react-router-dom'
+import "./Home.css"
 function Home() {
-  return <Link to="/article/1">Article link</Link>
+  return <main>
+    <section className="hero">
+    <div>
+      <h1>Stay updated on our latest <span>news</span>.</h1>
+      <p>Beyco supports you in connecting efficiently with existing and potential new coffee sellers and buyers, creating a track record online and making your coffee traceable from farmer to roaster. Connect today!</p>
+    </div>
+  </section>
+  
+  </main>
   
 }
 
